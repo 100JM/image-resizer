c2f6a5c4f0f056bbd317a24be3f539a1e85527e8
100JM <bjm_9310@naver.com>
2025-07-17 15:21:17 +0900

    v1.0.0 npm 배포

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+    testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
     collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
     coverageDirectory: 'coverage',
     testTimeout: 10000,
